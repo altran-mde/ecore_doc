25c513991dcdc0a4699e2d0bfd6a1c963608d8f4
CHARATSIDIS Vasileios <vcharatsidis@europe.corp.altran.com>
2018-04-13 16:04:06 +0200

    popup menu contain Generate documentation

---
+++ code/com.altran.general.ecoredoc.generator.integration.ui/src/com/altran/general/ecoredoc/generator/integration/ui/GenerateEcoreDocHandler.java
@@ -0,0 +1,15 @@
+package com.altran.general.ecoredoc.generator.integration.ui;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class GenerateEcoreDocHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+
+		return null;
+	}
+
+}

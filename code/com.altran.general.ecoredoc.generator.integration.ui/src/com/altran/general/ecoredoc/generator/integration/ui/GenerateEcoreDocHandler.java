105edf0f420cb823179b6c507d96b77006b50fcd
CHARATSIDIS Vasileios <vcharatsidis@europe.corp.altran.com>
2018-04-16 12:52:20 +0200

    Started to fill the execute method. Creation of ISelection object.

--- code/com.altran.general.ecoredoc.generator.integration.ui/src/com/altran/general/ecoredoc/generator/integration/ui/GenerateEcoreDocHandler.java
+++ code/com.altran.general.ecoredoc.generator.integration.ui/src/com/altran/general/ecoredoc/generator/integration/ui/GenerateEcoreDocHandler.java
@@ -3,12 +3,21 @@ package com.altran.general.ecoredoc.generator.integration.ui;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 public class GenerateEcoreDocHandler extends AbstractHandler {
-
+	private static final String EXTENSIONS = "ecore, xcore";
+	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-
+		
+		ISelection selection = HandlerUtil.getCurrentSelection(event);
+			
+		if (selection instanceof IStructuredSelection) {
+		} 
+			
 		return null;
 	}
 

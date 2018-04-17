f2d3ca6653b9b10a93636e290bfc45d2682c09d3
CHARATSIDIS Vasileios <vcharatsidis@europe.corp.altran.com>
2018-04-17 15:24:54 +0200

    Corrections in dependencies

--- code/com.altran.general.ecoredoc.generator.integration.ui/src/com/altran/general/ecoredoc/generator/integration/ui/GenerateEcoreDocHandler.java
+++ code/com.altran.general.ecoredoc.generator.integration.ui/src/com/altran/general/ecoredoc/generator/integration/ui/GenerateEcoreDocHandler.java
@@ -3,19 +3,52 @@ package com.altran.general.ecoredoc.generator.integration.ui;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 public class GenerateEcoreDocHandler extends AbstractHandler {
-	private static final String EXTENSIONS = "ecore, xcore";
+	private static enum Extension{
+		ecore,
+		xcore,
+		something
+	}
 	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		
 		ISelection selection = HandlerUtil.getCurrentSelection(event);
+		
+		boolean isStructuredSelection = selection instanceof IStructuredSelection;
 			
-		if (selection instanceof IStructuredSelection) {
+		if (isStructuredSelection) {
+			IStructuredSelection structuredSelection = (IStructuredSelection) selection;
+					
+			boolean isIFile = structuredSelection instanceof IFile;
+			
+			if(isIFile) {
+				IFile file = (IFile) structuredSelection;
+				
+				String fileExtension = file.getFileExtension();
+				
+				Extension ext = Extension.valueOf(fileExtension);
+				
+				switch(ext) {
+					case ecore:
+						break;
+						
+					case xcore:
+						break;
+				}
+				boolean isEcore = fileExtension.equals(Extension.ecore);
+				boolean isXcore = fileExtension.equals(Extension.xcore);
+					
+				if (isEcore || isXcore) {
+					
+				}
+			}
 		} 
 			
 		return null;

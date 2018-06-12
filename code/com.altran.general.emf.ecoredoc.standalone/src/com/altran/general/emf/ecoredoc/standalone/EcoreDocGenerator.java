47ae0e13b6efded28033252d0b26f30b838e79da
Niko Stotz <niko.stotz@altran.com>
2018-06-12 16:44:15 +0200

    added config options to standalone generator

--- code/com.altran.general.emf.ecoredoc.standalone/src/com/altran/general/emf/ecoredoc/standalone/EcoreDocGenerator.java
+++ code/com.altran.general.emf.ecoredoc.standalone/src/com/altran/general/emf/ecoredoc/standalone/EcoreDocGenerator.java
@@ -8,11 +8,15 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Consumer;
 
 import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 
+import com.altran.general.emf.ecoredoc.generator.config.ConfigFactory;
+import com.altran.general.emf.ecoredoc.generator.config.EcoreDocGeneratorConfig;
 import com.altran.general.emf.ecoredoc.util.EcoreDocUtils;
+import com.altran.general.emf.ecoredoc.util.EcoreMerger;
 
 public class EcoreDocGenerator {
 
@@ -22,6 +26,8 @@ public class EcoreDocGenerator {
 
 	private File outputFile;
 
+	private EcoreDocGeneratorConfig config;
+
 	public static void main(final String[] args) {
 		if (args.length > 0) {
 			final List<File> files = new ArrayList<>();
@@ -29,34 +35,72 @@ public class EcoreDocGenerator {
 			Boolean resolve = null;
 			File output = null;
 
+			final EcoreDocGeneratorConfig cfg = ConfigFactory.eINSTANCE.createEcoreDocGeneratorConfig();
+
 			for (int i = 0; i < args.length; i++) {
 				final String arg = args[i];
 				switch (arg.toLowerCase()) {
-				case "-r":
-				case "--resolve":
-					resolve = true;
-					break;
+					case "-r":
+					case "--resolve":
+						resolve = true;
+						break;
 
-				case "-o":
-				case "--output":
-					if (args.length > i) {
-						i++;
-						output = new File(args[i]);
-						if (output.isDirectory() || (output.exists() && !output.canWrite())) {
-							System.err.println("Cannot write to output " + output);
-							System.exit(-2);
+					case "-o":
+					case "--output":
+						if (args.length > i) {
+							i++;
+							output = new File(args[i]);
+							if (output.isDirectory() || (output.exists() && !output.canWrite())) {
+								System.err.println("Cannot write to output " + output);
+								System.exit(-2);
+							}
 						}
-					}
-					break;
+						break;
 
-				default:
-					final File file = new File(arg);
-					if (file.canRead() && file.isFile()) {
-						files.add(file);
-					} else {
-						System.err.println("Cannot read, ignoring input " + file);
-					}
-					break;
+					case "-defaults":
+						cfg.setRenderDefaults(false);
+						break;
+					case "+defaults":
+						cfg.setRenderDefaults(true);
+
+					case "-bounds":
+						cfg.setRenderBounds(false);
+						break;
+					case "+bounds":
+						cfg.setRenderBounds(true);
+
+					case "-inherited":
+						cfg.setRepeatInherited(false);
+						break;
+					case "+inherited":
+						cfg.setRepeatInherited(true);
+
+					case "-usecases":
+						cfg.setRenderUseCases(false);
+						break;
+					case "+usecases":
+						cfg.setRenderUseCases(true);
+
+					case "--positionedatatypes":
+						i = setInt(args, i, pos -> cfg.setPositionEDataTypes(pos));
+						break;
+
+					case "--positioneenums":
+						i = setInt(args, i, pos -> cfg.setPositionEEnums(pos));
+						break;
+
+					case "--positioneclasses":
+						i = setInt(args, i, pos -> cfg.setPositionEClasses(pos));
+						break;
+
+					default:
+						final File file = new File(arg);
+						if (file.canRead() && file.isFile()) {
+							files.add(file);
+						} else {
+							System.err.println("Cannot read, ignoring input " + file);
+						}
+						break;
 				}
 			}
 
@@ -70,6 +114,10 @@ public class EcoreDocGenerator {
 
 					if (output != null) {
 						ecoreDocGenerator.setOutputFile(output);
+					}
+
+					if (cfg != null) {
+						ecoreDocGenerator.setConfig(cfg);
 					}
 
 					final File result = ecoreDocGenerator.generate();
@@ -86,41 +134,83 @@ public class EcoreDocGenerator {
 		}
 	}
 
+	protected static int setInt(final String[] args, final int i, final Consumer<Integer> setter) {
+		int result = i;
+
+		if (args.length > result) {
+			result++;
+			try {
+				final int pos = Integer.parseInt(args[result]);
+				setter.accept(pos);
+			} catch (final NumberFormatException e) {
+				// do nothing
+			}
+		}
+
+		return result;
+	}
+
 	private static void printUsage() {
-		System.out.println("Generates reference documentation for ecore models.\n" + 
-				"\n" + 
-				"The output is inspired by JavaDoc and formatted in AsciiDoctor format.\n" + 
-				"AsciiDoctor can easily be rendered to HTML, PDF, or Eclipse help.\n" + 
-				"\n" + 
-				"\n" + 
-				"Usage:\n" + 
-				"\n" + 
-				"EcoreDocGenerator [parameters] [List of ecore files to generate]\n" + 
-				"\n" + 
-				"If unspecified, the output file name will be \"<firstEcoreFile.ecore>.adoc\"\n" + 
-				"\n" + 
-				"\n" + 
-				"Parameters:\n" + 
-				"\n" + 
-				"  -r,\n" + 
-				"  --resolve: Resolve external references\n" + 
-				"  \n" + 
-				"  -o <outputFile>,\n" + 
-				"  --output <outputFile>: Specifiy output file name.\n" + 
-				"\n" + 
-				"\n" + 
-				"Examples:\n" + 
-				"\n" + 
-				"  EcoreDocGenerator my.ecore\n" + 
-				"  Generates the documentation of my.ecore into my.ecore.adoc\n" + 
-				"  \n" + 
-				"  EcoreDocGenerator some/path/to/my.ecore other.ecore\n" + 
-				"  Generates the documentation of some/path/to/my.ecore and other.ecore into some/path/to/my.ecore.adoc\n" + 
-				"  \n" + 
-				"  EcoreDocGenerator -r my.ecore\n" + 
-				"  Tries to resolve all external references in my.ecore and generates the documentation of my.ecore and referenced models into my.ecore.adoc\n" + 
-				"  \n" + 
-				"  EcoreDocGenerator -o output.adoc my.ecore other.ecore\n" + 
+		System.out.println("Generates reference documentation for ecore models.\n" +
+				"\n" +
+				"The output is inspired by JavaDoc and formatted in AsciiDoctor format.\n" +
+				"AsciiDoctor can easily be rendered to HTML, PDF, or Eclipse help.\n" +
+				"\n" +
+				"\n" +
+				"Usage:\n" +
+				"\n" +
+				"EcoreDocGenerator [parameters] [List of ecore files to generate]\n" +
+				"\n" +
+				"If unspecified, the output file name will be \"<firstEcoreFile.ecore>.adoc\"\n" +
+				"\n" +
+				"\n" +
+				"Parameters:\n" +
+				"\n" +
+				"  -r,\n" +
+				"  --resolve: Resolve external references\n" +
+				"  \n" +
+				"  -o <outputFile>,\n" +
+				"  --output <outputFile>: Specify output file name.\n" +
+				"  \n" +
+				"  --positionEDataTypes <pos>: Set rendering position of all EDataTypes within EPackage\n" +
+				"  \n" +
+				"  --positionEEnums <pos>:     Set rendering position of all EEnums within EPackage\n" +
+				"  \n" +
+				"  --positionEClasses <pos>:   Set rendering position of all EClasses within EPackage\n" +
+				"  \n" +
+				"  [+|-]defaults:  [Enable|disable] rendering of default values\n" +
+				"  \n" +
+				"  [+|-]bounds:    [Enable|disable] rendering of multiplicity bounds (overwrites defaults parameter)\n"
+				+
+				"  \n" +
+				"  [+|-]inherited: [Enable|disable] repetition of inherited features\n" +
+				"  \n" +
+				"  [+|-]useCases:  [Enable|disable] rendering of use cases (references to other usages of this element)\n"
+				+
+				"\n" +
+				"\n" +
+				"Examples:\n" +
+				"\n" +
+				"  EcoreDocGenerator my.ecore\n" +
+				"  Generates the documentation of my.ecore into my.ecore.adoc\n" +
+				"  \n" +
+				"  EcoreDocGenerator some/path/to/my.ecore other.ecore\n" +
+				"  Generates the documentation of some/path/to/my.ecore and other.ecore into some/path/to/my.ecore.adoc\n"
+				+
+				"  \n" +
+				"  EcoreDocGenerator -r my.ecore\n" +
+				"  Tries to resolve all external references in my.ecore and generates the documentation of my.ecore and referenced models into my.ecore.adoc\n"
+				+
+				"  \n" +
+				"  EcoreDocGenerator -defaults +bounds my.ecore\n" +
+				"  Generates the documentation of my.ecore and referenced models into my.ecore.adoc without rendering default values, but still rendering multiplicity bounds\n"
+				+
+				"  \n" +
+				"  EcoreDocGenerator --positionEClasses 1 --positionEEnums 2 --positionEDataTypes 3 my.ecore\n" +
+				"  Generates the documentation of my.ecore and referenced models into my.ecore.adoc with all EClasses first, then all EEnums, and finally all EDataTypes\n"
+				+
+				"  \n" +
+				"  EcoreDocGenerator -o output.adoc my.ecore other.ecore\n" +
 				"  Generates the documentation of my.ecore and other.ecore into output.adoc\n");
 	}
 
@@ -139,7 +229,7 @@ public class EcoreDocGenerator {
 		final ResourceSetImpl resourceSet = EcoreDocUtils.getInstance().createResourceSet();
 
 		EcoreDocUtils.getInstance().loadInputModels(resourceSet, this.inputFiles);
-		
+
 		EcoreDocUtils.getInstance().resolve(resourceSet, shouldResolve());
 
 		final Set<EClassifier> classifiers = EcoreDocUtils.getInstance().collectInput(resourceSet);
@@ -162,6 +252,10 @@ public class EcoreDocGenerator {
 	private CharSequence generate(final Set<EClassifier> classifiers) {
 		final com.altran.general.emf.ecoredoc.generator.EcoreDocGenerator generator = new com.altran.general.emf.ecoredoc.generator.EcoreDocGenerator(
 				classifiers);
+
+		if (getConfig() != null) {
+			new EcoreMerger<>(generator.getConfig()).merge(getConfig());
+		}
 
 		final CharSequence result = generator.generate();
 		return result;
@@ -186,5 +280,13 @@ public class EcoreDocGenerator {
 
 	public void setOutputFile(final File outputFile) {
 		this.outputFile = outputFile;
+	}
+
+	public EcoreDocGeneratorConfig getConfig() {
+		return this.config;
+	}
+
+	public void setConfig(final EcoreDocGeneratorConfig config) {
+		this.config = config;
 	}
 }
